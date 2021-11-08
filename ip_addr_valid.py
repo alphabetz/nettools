b15b32883bb6a4811b499a07b7b5f6d58dab3a49
alphabetz <a.alphabet@gmail.com>
2021-11-08 16:22:36 +0700

    check if ip addresses are valid

---
+++ ip_addr_valid.py
@@ -0,0 +1,21 @@
+import sys
+import ipaddress
+
+#Check ip addresses are not in reserved IP"
+
+def ip_addr_valid(list):
+    for ip in list:
+        if (
+            ipaddress.ip_address(ip).is_multicast
+            or ipaddress.ip_address(ip).is_global
+            or ipaddress.ip_address(ip).is_unspecified
+            or ipaddress.ip_address(ip).is_reserved
+            or ipaddress.ip_address(ip).is_loopback
+            or ipaddress.ip_address(ip).is_link_local
+        ):
+            print("There is invalid ip!",ip)
+            sys.exit()
+
+if __name__ == "__main__":
+    list = ["192.168.1.0","192.168.5.4","127.0.0.1"]
+    ip_addr_valid(list)
