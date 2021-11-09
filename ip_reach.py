439cc6c7a5b1c0a93cd2a43a6c40bb8417f10fc2
alphabetz <a.alphabet@gmail.com>
2021-11-09 14:11:13 +0700

    check ip reachability

---
+++ ip_reach.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+
+# Check IP reachability
+
+def ip_reach(list):
+    # OS verification
+    ping_option = '-c 2'
+    try:
+        subprocess.run("uname", stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    except FileNotFoundError:
+        ping_option = '/n 2'
+    for ip in list:
+        print("Pinging "+ ip )
+        reply = subprocess.run(["ping", ping_option, ip],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+        if reply.returncode == 0:
+            print("\n* {} is reachable\n".format(ip))
+            continue
+        else:
+            print("\n* {} is not reachable, Check ip address and try again.".format(ip))
+            sys.exit()
+
+if __name__ == "__main__":
+    list = ["192.168.1.1","192.168.5.4","127.0.0.1"]
+    ip_reach(list)
+
