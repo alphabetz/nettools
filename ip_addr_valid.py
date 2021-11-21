6e71568ff59260c1c9285a8b768e2dc1ea01b0d0
alphabetz <a.alphabet@gmail.com>
2021-11-21 20:12:55 +0700

    Add error handling, change to ip_network function for support new program

--- ip_addr_valid.py
+++ ip_addr_valid.py
@@ -4,19 +4,23 @@ import ipaddress
 #Check ip addresses are not in reserved IP"
 
 def ip_addr_valid(list):
-    for ip in list:
-        ip = ip.rstrip()
-        if (
-            ipaddress.ip_address(ip).is_multicast
-            or ipaddress.ip_address(ip).is_global
-            or ipaddress.ip_address(ip).is_unspecified
-            or ipaddress.ip_address(ip).is_reserved
-            or ipaddress.ip_address(ip).is_loopback
-            or ipaddress.ip_address(ip).is_link_local
-        ):
-            print("There is invalid ip!",ip)
-            sys.exit()
+    try:
+        for ip in list:
+            ip = ip.rstrip()
+            if (
+                ipaddress.ip_network(ip, strict=False).is_multicast
+                or ipaddress.ip_network(ip, strict=False).is_global
+                or ipaddress.ip_network(ip, strict=False).is_unspecified
+                or ipaddress.ip_network(ip, strict=False).is_reserved
+                or ipaddress.ip_network(ip, strict=False).is_loopback
+                or ipaddress.ip_network(ip, strict=False).is_link_local
+            ):
+                print("The ip {0} is a restricted IPv4 in the list please recheck".format(ip))
+                sys.exit()
+    except ValueError as err:
+        print("Value error: {0}".format(err))
+        sys.exit()
 
 if __name__ == "__main__":
-    list = ["192.168.1.0","192.168.5.4","127.0.0.1"]
+    list = ["192.168.1.9/24","192.168.5.4", "202.22.0.66"]
     ip_addr_valid(list)
